bdd5a0556f24ed1be83829bab996eb1b7e95450e
eveem <monpriya.tammavong@gmail.com>
2017-09-13 05:22:04 +0700

    Yellow and orange can't detect

--- detect_by_range.py
+++ detect_by_range.py
@@ -4,8 +4,8 @@ import numpy as np
 img = cv2.imread('ex2.png')
 cimg = cv2.imread('ex2.png')
 
-color = img[250, 220]
-cv2.circle(cimg, (250, 220), 2, (0, 0, 255), 3)
+color = img[290, 420]
+cv2.circle(cimg, (290, 420), 2, (0, 0, 255), 3)
 print color
 
 cv2.imshow('rgb', cimg)
--- detect_circle.py
+++ detect_circle.py
@@ -1,36 +1,33 @@
 import cv2
 import numpy as np
 
-inner_blue = np.array([205, 138, 56])
-outer_blue = np.array([163, 93, 18])
+lower_blue = np.array([205, 138, 56])
+upper_blue = np.array([163, 93, 18])
 
-inner_purple = np.array([220, 217, 118])
-outer_purple = np.array([206, 192, 95])
+lower_lblue = np.array([206, 192, 95])
+upper_lblue = np.array([220, 217, 118])
 
-inner_yellow = np.array([91, 200, 229])
-outer_yellow = np.array([103, 196, 207])
+# inner_yellow = np.array([91, 200, 229])
+# outer_yellow = np.array([103, 196, 207])
 
-inner_pink = np.array([56, 73, 225])
-outer_pink = np.array([48, 48, 187])
+lower_green = np.array([54, 184, 133])
+upper_green = np.array([93, 202, 167])
 
-inner_green = np.array([93, 202, 167])
-outer_green = np.array([54, 184, 133])
+# lower_orange = np.array([80, 97, 232])
+# upper_orange = np.array([92, 92, 205])
 
-inner_lblue = np.array([200, 73, 160])
-outer_lblue = np.array([216, 113, 190])
+upper_red = np.array([83, 92, 236])
+lower_red = np.array([58, 58, 191])
 
-inner_orange = np.array([93, 202, 167])
-outer_orange = np.array([67, 179, 134])
-
-inner_red = np.array([83, 92, 236])
-outer_red = np.array([58, 58, 191])
+lower_pink = np.array([158, 62, 203])
+upper_pink = np.array([196, 120, 237])
 
 img = cv2.imread('ex2.png', 0)
 
 cimg = cv2.imread('ex2.png')
 
 # hsv = cv2.cvtColor(cimg, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(cimg, outer_green, inner_green)
+mask = cv2.inRange(cimg, lower_orange, upper_orange)
 res = cv2.bitwise_and(cimg, cimg, mask= mask)
 
 circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 50, np.array([]), 1, 73, 20, 50)
